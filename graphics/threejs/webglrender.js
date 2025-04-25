050f1c7e3b4747fdca5db1c3abae2188758a27bd
wuyifan0203 <1208097313@qq.com>
2025-04-25 15:23:38 +0800

    update process

--- graphics/threejs/webglrender.js
+++ graphics/threejs/webglrender.js
@@ -1,5 +1,13 @@
 /*
  * @Author: wuyifan0203 1208097313@qq.com
+ * @Date: 2024-10-12 10:25:28
+ * @LastEditors: wuyifan0203 1208097313@qq.com
+ * @LastEditTime: 2025-04-25 15:23:22
+ * @FilePath: \Obsiden\graphics\threejs\webglrender.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+/*
+ * @Author: wuyifan0203 1208097313@qq.com
  * @Date: 2024-02-29 15:45:49
  * @LastEditors: wuyifan0203 1208097313@qq.com
  * @LastEditTime: 2025-03-05 14:32:09
